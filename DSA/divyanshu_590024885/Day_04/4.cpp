ccf20edcec37de0c97e229242e341f385ca57bea
divyanshu11-vyas <iamdiv2006@gmail.com>
2026-06-18 21:11:57 +0530

    create day4

---
+++ DSA/divyanshu_590024885/Day_04/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include<vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;  // handle k > n
+        
+        reverse(nums.begin(), nums.end());           
+        reverse(nums.begin(), nums.begin() + k);     
+        reverse(nums.begin() + k, nums.end());       
+    }
+};
